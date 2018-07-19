daeb0ca89bb4904c7b32163eac182c283ddcdc68
Nicholas Leask <leask.nicholas@gmail.com>
2018-07-19 11:04:26 -0400

    fixing lint errors

--- migrations/20180716120152_user.js
+++ migrations/20180716120152_user.js
@@ -1,15 +1,15 @@
 
-exports.up = function(knex, Promise) {
+exports.up = function(knex) {
   return knex.schema.createTable('users', (table) => {
-      table.increments(); 
-      table.text('facebookId').unique().notNullable();
-      table.text('email').unique().notNullable();
-      table.text('profilePic').notNullable();
-      table.text('displayName').notNullable();
-      table.text('accessToken').notNullable();  
+    table.increments(); 
+    table.text('facebookId').unique().notNullable();
+    table.text('email').unique().notNullable();
+    table.text('profilePic').notNullable();
+    table.text('displayName').notNullable();
+    table.text('accessToken').notNullable();  
   });
 };
 
-exports.down = function(knex, Promise) {
+exports.down = function(knex) {
   return knex.schema.dropTable('users');
 };
--- migrations/20180718140449_bets.js
+++ migrations/20180718140449_bets.js
@@ -1,20 +1,20 @@
 
-exports.up = function(knex, Promise) {
+exports.up = function(knex) {
   return knex.schema.createTable('bets',(table)=>{
-      table.increments();
-      table.integer("creatorId").unsigned().notNullable().references("id").inTable("users").onDelete("cascade");
-      table.integer("receiverId").unsigned().notNullable().references("id").inTable("users").onDelete("cascade");
-      table.text("title").notNullable();
-      table.text("description").notNullable();
-      table.datetime("startDate").notNullable();
-      table.datetime("endDate").notNullable();
-      table.text("wager").notNullable();
-      table.text("creatorVoteWinner");
-      table.text("receiverVoteWinner");
-      table.boolean("receiverAccepted").default(false);
-  })
+    table.increments();
+    table.integer('creatorId').unsigned().notNullable().references('id').inTable('users').onDelete('cascade');
+    table.integer('receiverId').unsigned().notNullable().references('id').inTable('users').onDelete('cascade');
+    table.text('title').notNullable();
+    table.text('description').notNullable();
+    table.datetime('startDate').notNullable();
+    table.datetime('endDate').notNullable();
+    table.text('wager').notNullable();
+    table.text('creatorVoteWinner');
+    table.text('receiverVoteWinner');
+    table.boolean('receiverAccepted').default(false);
+  });
 };
 
-exports.down = function(knex, Promise) {
-  return knex.schema.dropTable("bets");
+exports.down = function(knex) {
+  return knex.schema.dropTable('bets');
 };
